285a60a137d11b5668ea4cced8be6e055df792d2
Lareciyo <lmcneal@perseverenow.org>
2025-06-03 14:19:21 -0500

    first dredded comment

--- main.js
+++ main.js
@@ -1,44 +1,111 @@
-function isEvenlyDivisible() {
-
+function isEvenlyDivisible(a, b) {
+return a % b === 0
 }
 
-function halfSquare() {
+function halfSquare(num) {
+  return num ** 2 / 2
 }
 
-function isLong() {
+function isLong(str) {
+  return str.length >= 15;
 }
 
-function exclaim() {
+function exclaim(str) {
+  const splitStr = str.split("")
+   for(let i = str.length - 1; i >= 0; i--){
+        if(str[i] !== "!"){
+      return str +"!"
+    }else if(splitStr[i - 1] === "!"){
+      splitStr.pop()
+    }else{
+      return splitStr.join("")
+    }
+    }
+  }
+      
+      
+exclaim("NOOOOOOO, WAAAAYYYYYY")
+
+function countWords(str) {
+  let words = str.split(' ')
+  return words.length
 }
 
-function countWords() {
+function containsDigit(str) {
+  for(const char of str){
+    if(char >= '0' && char <= "9"){
+      return true
+    }
+  }
+  return false
 }
 
-function containsDigit() {
+function containsLowerCase(str) {
+for(let char of str){
+  if(char === char.toLowerCase() && char !== char.toUpperCase()){
+    return true
+  }
+}
+return false
 }
 
-function containsLowerCase() {
+function containsUpperCase(str) {
+for(let char of str){
+  if(char === char.toUpperCase() && char !== char.toLowerCase()){
+    return true
+  }
+}
+return false
 }
 
-function containsUpperCase() {
+function containsNonAlphanumeric(str) {
+  for(let char of str){
+    if(!char.match(/[a-z0-9]/i)){
+      return true
+    }
+  }
+  return false
 }
 
-function containsNonAlphanumeric() {
+function containsSpace(str) {
+  for(let char of str){
+    if(char === ' '){
+      return true
+    }
+  }
+  return false
 }
 
-function containsSpace() {
+function digits(num) {
+  let numStr = Math.abs(num).toString()
+  let digits = []
+  for(let char of numStr){
+    if(!isNaN(char)){
+      digits.push(parseInt(char))
+    }
+  }
+  return digits
 }
 
-function digits() {
+function truncate(str){
+  return str.length >= 15 ? str.slice(0, 8) + "..." : str;
 }
 
-function truncate() {
+
+function isValidPassword(str) {
+  return containsLowerCase(str) && containsUpperCase(str) && containsDigit(str) && containsNonAlphanumeric(str) && !containsSpace(str)
 }
 
-function isValidPassword() {
-}
 
-function onlyPunchy(arr) {
+function onlyPunchy(titles) {
+  let result = []
+  for(let title of titles){
+    let trimmedTitle = title.replace(/!+/g, '')
+    if(trimmedTitle.length < 15){
+      result.push(trimmedTitle + '!')
+    }
+  }
+  return result
 }
 
 
